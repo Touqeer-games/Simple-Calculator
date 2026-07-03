f4b0aff6f040b9cab2034c0f586a4ac3ef4127a8
Touqeer-games <Touqu09@gmail.com>
2026-07-03 14:01:12 +0500

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,13 @@
+# Simple Calculator
+num1 = 15
+num2 = 6
+
+add = num1 + num2
+sub = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+print("---SIMPLE CALCULATOR---")
+print(f" {num1} + {num2} = {add}")
+print(f" {num1} - {num2} = {sub}")
+print(f" {num1} * {num2} = {multiply}")
+print(f" {num1} / {num2} = {divide}")
